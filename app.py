776324e417b8afcb8c3e37ae5a1675b8a6b24d7d
bogal2006 <51925338+bogal2006@users.noreply.github.com>
2022-12-06 22:03:42 +0500

    second

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, redirect, url_for, request, session
-from markupsafe import escape
 import datetime
 
 app = Flask(__name__)
@@ -14,56 +13,35 @@ def auth_method():
     return redirect(url_for('main_2'))
 
 
+@app.route('/change')
+def change_ad():
+    if 'auth' in session:
+        return '<h1>Измените объявление </h1> <form action="/auth" method ="post"><input name="user"><input ' \
+               'type="submit"></form> '
+    else:
+        return redirect(url_for('main_2'))
+
+
+@app.route('/delete')
+def delete_ad():
+    if 'auth' in session:
+        session.pop('auth', None)
+        return redirect(url_for('main_2'))
+    else:
+        redirect(url_for('main_2'))
+
+
 @app.route('/')
 def main_2():
     if 'auth' not in session:
-        return '<form action="/auth" method ="post"><input name="user"><input type="submit"></form>'
+        return '<h1>Введите объявление </h1> <form action="/auth" method ="post"><input name="user"><input ' \
+               'type="submit"></form> <h3>Изменить объявление в URL (адресную строку) добавить /change </h3>' \
+               '<h3>Удалить объявление в URL(адресную строку) добавить /delete </h3> '
     else:
         u = session['auth']
-        return f'Hello {u}'
+        return f'<h1>Объявление </h1> {u}<h3>Изменить объявление в URL (адресную строку) добавить /change </h3>' \
+               '<h3>Удалить объявление в URL(адресную строку) добавить /delete </h3>'
 
-
-"""
-@app.get('/')
-def counter():
-    if 'k' not in session:
-        session['k'] = 0
-    else:
-        session['k'] = session['k'] + 1
-    k = session['k']
-    return f'зашли {k} раз'
-
-
-
-@app.route('/', methods=['GET', 'POST'])
-def first_web():
-    if request.method == "POST":
-        return '<h1>Hello World! (POST) </h1>'
-    else:
-        return '<h1> Hello World! (GET) </h1> <form method="POST"><input type="submit"</form>'
-
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-@app.route('/user/<int:user_id>')
-def test(user_id):
-    return f'user n{user_id}'
-
-
-@app.route('/redir')
-def redir():
-    return redirect(url_for('hello_world'))
-
-
-
-@app.route('/<name_1>')
-def hello(name_1):
-    return f'hello{escape(name_1)}'
-"""
 
 if __name__ == '__main__':
     app.run()
